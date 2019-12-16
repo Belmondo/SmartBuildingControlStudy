9c2d489f16054f0a759beb4c51975b121b1961c4
Belmondo Rodrigues <belmondo.rodrigues@gmail.com>
2019-12-15 21:04:16 -0300

    Melhoria da classe de métrica

--- app/src/main/java/great/android/cmu/ubiapp/helpers/CalculateMetrics.java
+++ app/src/main/java/great/android/cmu/ubiapp/helpers/CalculateMetrics.java
@@ -15,7 +15,6 @@ public class CalculateMetrics {
 
     static int numberOfRulesVerified = 0;
 
-
     public static void calculateSingleWat(String adaptationName, Long WorkingTime, Long AdaptivityTime){
         System.out.println("Name of Adaptation: " + adaptationName + " the WAT calculus = " + (WorkingTime/AdaptivityTime));
         Log.d("WAT", "Name of Adaptation: " + adaptationName + " the WAT calculus = " + (WorkingTime/AdaptivityTime));
@@ -27,38 +26,27 @@ public class CalculateMetrics {
         Toast.makeText(context,"Name of Adaptation: " + adaptationName + " the WAT calculus = " + (WorkingTime/AdaptivityTime), Toast.LENGTH_LONG).show();
         System.out.println("Name of Adaptation: " + adaptationName + " the WAT calculus = " + (WorkingTime/AdaptivityTime));
         Log.d("WAT", "Name of Adaptation: " + adaptationName + " the WAT calculus = " + (WorkingTime/AdaptivityTime));
-
-
-
     }
-
-
 
 
     public static void calculateGeneralWat(){
 
         Long GeneralWATTimeVariable = null;
-
         for(int i=0; i< GeneralWatTimes.size(); i++){
             GeneralWATTimeVariable +=GeneralWatTimes.get(i);
-
 
         }
 
         System.out.println("General Wat Calculated: " + GeneralWATTimeVariable );
         Log.d("GENERAL WAT", "General Wat Calculated: " + GeneralWATTimeVariable );
-
     }
 
 
     public static void calculateGeneralWat(Context context){
 
         Long GeneralWATTimeVariable = null;
-
         for(int i=0; i< GeneralWatTimes.size(); i++){
             GeneralWATTimeVariable +=GeneralWatTimes.get(i);
-
-
         }
 
         Toast.makeText(context,"General Wat Calculated: " + GeneralWATTimeVariable, Toast.LENGTH_LONG).show();
@@ -74,17 +62,13 @@ public class CalculateMetrics {
 
 
     public static void setTATimes(Long taTimeOnThisMoment){
-
         TaTimes.add(taTimeOnThisMoment);
     }
 
     public static void calculateTA(){
         Long GeneralTAVariable = null;
-
         for(int i=0; i< TaTimes.size(); i++){
-            GeneralTAVariable +=GeneralWatTimes.get(i);
-
-
+            GeneralTAVariable +=TaTimes.get(i);
         }
 
         System.out.println("General TA Calculated: " + GeneralTAVariable );
@@ -94,17 +78,20 @@ public class CalculateMetrics {
 
     public static void calculateTA(Context context){
         Long GeneralTAVariable = null;
-
         for(int i=0; i< TaTimes.size(); i++){
             GeneralTAVariable +=GeneralWatTimes.get(i);
-
-
         }
 
         Toast.makeText(context,"General TA Calculated: " + GeneralTAVariable, Toast.LENGTH_LONG).show();
         System.out.println("General TA Calculated: " + GeneralTAVariable );
         Log.d("GENERAL TA", "General TA Calculated: " + GeneralTAVariable );
+    }
 
+    public static void printAllTA(){
+        for(int i=0;i<TaTimes.size();i++) {
+            System.out.println(TaTimes.get(i));
+            Log.d("PRINT ALL TA", "ALL TA LIST: " + TaTimes.get(i) );
+        }
     }
 
     public static void setNumberOfRulesVerified(){
@@ -114,6 +101,4 @@ public class CalculateMetrics {
     public int getNumberOfRulesVerified(){
         return this.numberOfRulesVerified;
     }
-
-
 }
